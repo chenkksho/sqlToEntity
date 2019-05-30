e54e7efd2acf0835a4968d0a80d87e4458d20b2d
chenkksho <530910789@qq.com>
2019-05-30 09:47:11 +0800

    更改tab页Title

--- sqlToEntity/Form1.Designer.cs
+++ sqlToEntity/Form1.Designer.cs
@@ -78,7 +78,7 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Size = new System.Drawing.Size(852, 343);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "初始sql";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // txtSourceTxt
